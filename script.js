6d68c53572ff9961e3e2613c053c061f553b196d
Nehal2027 <nehalbaaji@gmail.com>
2026-09-20 23:04:18 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuBtn = document.getElementById("menuBtn");
+
+menuBtn.addEventListener("click", () => {
+  alert("আপনার Menu এখানে যোগ করা যাবে।");
+});
+
+// নিজের লিংক বসাতে index.html-এর href পরিবর্তন করুন।
